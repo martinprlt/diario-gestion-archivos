ff065490c544b0c1b3a476264ad606048d170aaf
unknown <martinprlt02@gmail.com>
2025-10-26 06:58:35 -0300

    fix: corregir todas las importaciones de chatContext a ChatContext

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
-import { useChat } from "../context/chatContext.jsx";
+import { useChat } from "../context/ChatContext.jsx";
 import { AuthContext } from "../context/AuthContext.js";
 import "../assets/styles/userlist.css"; // nuevo archivo para detalles visuales
 
