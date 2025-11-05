2a1d231540d27b69dc5776fffec26d69861d3873
unknown <martinprlt02@gmail.com>
2025-11-05 15:00:36 -0300

    sfdgsdsddsasd

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 // server.js (EN LA RAIZ del proyecto)
 import 'dotenv/config.js';
-import app from './app.js';  // ← CORREGIDO: './src/app.js' no '/app/src/src/app.js'
-import { testDB } from './config/db.js';
+import app from './src/app.js';  // ← CORREGIDO: './src/app.js' no '/app/src/src/app.js'
+import { testDB } from './src/config/db.js';
 import http from 'http';
-import { initChatServer } from './chat/chat.server.js';
+import { initChatServer } from './src/chat/chat.server.js';
 
 const PORT = process.env.PORT || 5000;
 
